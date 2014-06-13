1e9b8bd83017b9b938f9156ce8215d60785515b3
danieltroger <daniel@natur-kultur.eu>
2014-06-13 20:02:59 +0200

    Update toolz.c

--- toolz.c
+++ toolz.c
@@ -1,3 +1,5 @@
+//utility file of the sn0wbreak project
+
 char *sbfile(char *filename) // tested, worx
 {
   /* example usage:
@@ -8,4 +10,13 @@ char *sbfile(char *filename) // tested, worx
   char *file = strcat(homedir, "/.sn0wbreak/");
   file = strcat(file, filename);
   return file;
+}
+
+/* DFU mode */
+void dfu_init(void)
+{
+    info("Put your device in DFU.\n if you do not know what DFU mode is do not attempt this\n");
+    sleep(260);
+    info("device should be in DFU mode by now\n");
+    info("Done\n");
 }
