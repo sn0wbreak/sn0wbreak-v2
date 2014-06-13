c1f82dd3440d616bff53e23145fd88503e5ac6fa
danieltroger <daniel@natur-kultur.eu>
2014-06-13 19:59:46 +0200

    better

--- sn0wbreak(2).c
+++ sn0wbreak(2).c
@@ -5,22 +5,11 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "tollz.c"
 //#include "core.h"
 //#include "payload.h"
 //#include <libusb-1.0/libusb.h>
 
-char *sbfile(char *filename) // tested, worx
-{
-  /* example usage:
-  char *testfile = sbfile("res.zip");
-  printf("%s",testfile);
-  return 0;
- */
-  char *homedir = getenv("HOME");
-  char *file = strcat(homedir, "/.sn0wbreak/");
-  file = strcat(file, filename);
-  return file;
-}
 int main ()
 {
 }
