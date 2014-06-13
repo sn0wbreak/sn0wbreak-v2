33e0dba2b33810f46a37976c577f88f122b1c4cd
danieltroger <daniel@natur-kultur.eu>
2014-06-13 20:01:11 +0200

    Update toolz.c

--- toolz.c
+++ toolz.c
@@ -3,7 +3,6 @@ char *sbfile(char *filename) // tested, worx
   /* example usage:
   char *testfile = sbfile("res.zip");
   printf("%s",testfile);
-  return 0;
  */
   char *homedir = getenv("HOME");
   char *file = strcat(homedir, "/.sn0wbreak/");
