80ac03529b8ea112b011e0ecbd8456771a660390
danieltroger <daniel@natur-kultur.eu>
2014-06-13 19:51:55 +0200

    rewrite version (shall be cleaner)

---
+++ sn0wbreak(2).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "core.h"
+#include "payload.h"
+#include <libusb-1.0/libusb.h>
