3f72349f7511c1c9ef49498a2c5ebb83536809d4
danieltroger <daniel@natur-kultur.eu>
2014-06-13 20:35:22 +0200

    Update sn0wbreak-2.c

--- sn0wbreak-2.c
+++ sn0wbreak-2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <inttypes.h>
 #include "toolz.c"
 #include "partial.c"
-//#include "core.h"
-//#include "payload.h"
 
 int main ()
 {
