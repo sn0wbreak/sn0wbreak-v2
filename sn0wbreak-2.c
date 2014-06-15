941b4373f9094ad7f2e3264d5e9226a3422bd36c
danieltroger <daniel@natur-kultur.eu>
2014-06-15 13:44:58 +0200

    Update sn0wbreak-2.c

--- sn0wbreak-2.c
+++ sn0wbreak-2.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include "toolz.c"
+  #include <stdio.h>
+  #include <sys/stat.h>
+  #include <sys/types.h>
+  #include <ctype.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <stdbool.h>
+  #include <unistd.h>
+  #include <stdint.h>
+  #include <inttypes.h>
+  #include "toolz.c"
 
-int main ()
-{
-// Do something
-}
+  int main ()
+  {
+    // Do something
+    printf("%s | %s",sbfile("first"),sbfile("second"));
+  }
