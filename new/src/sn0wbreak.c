22e5de32abe0fe1c321482b52ba2a345fe7f2233
PythEch <pythech.tr@gmail.com>
2014-06-16 13:32:53 +0300

    New build directory

---
+++ new/src/sn0wbreak.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include "libirecovery.h"
+
+int main ()
+{
+   return 0;
+}
