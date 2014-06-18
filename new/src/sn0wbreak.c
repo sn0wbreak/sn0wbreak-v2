004eb0d937ef0933b890b7465862e82f5182d47c
PythEch <pythech.tr@gmail.com>
2014-06-18 20:28:44 +0300

    Competition is good :+1:

--- new/src/sn0wbreak.c
+++ new/src/sn0wbreak.c
@@ -16,26 +16,31 @@
 
 int main(int argc, char **argv)
 {
-	irecv_init();
-	irecv_client_t client = NULL;
-	if (irecv_open_with_ecid_and_attempts(&client, 0, 5) != IRECV_E_SUCCESS)
-	{
-		ERROR("Could not connect to iDevice! Make sure you are in DFU mode...\n");
-		return -1;
-	}
-	irecv_device_t device = NULL;
-	if (irecv_devices_get_device_by_client(client, &device) != IRECV_E_SUCCESS)
-	{
-		ERROR("Something went wrong, could not get iDevice info!\n");
-		return -1;
-	}
-        const struct irecv_device_info *info = irecv_get_device_info(client);
-        if (!info)
-        {
-          ERROR("Could not get iDevice info!\n");
-          return -1;
-        }
-	printf("Product Type: %s\nHardware Model: %s\nBoard ID: %d\nChip ID: %d\nECID: %"PRId64" (decimal), %"PRIX64" (hexadecimal)\nIMEI: %s\n", device->product_type, device->hardware_model, device->board_id, device->chip_id,info->ecid, info->ecid,info->imei);
+    irecv_init();
+    irecv_client_t client = NULL;
+    if (irecv_open_with_ecid_and_attempts(&client, 0, 5) != IRECV_E_SUCCESS)
+    {
+        ERROR("Could not connect to iDevice! Make sure you are in DFU mode...\n");
+        return -1;
+    }
 
+    irecv_device_t device = NULL;
+    if (irecv_devices_get_device_by_client(client, &device) != IRECV_E_SUCCESS)
+    {
+        ERROR("Something went wrong, could not get device!\n");
+        return -1;
+    }
+    DEBUG("Connected to %s (Model: %s, Board ID: 0x%02x, Chip ID: 0x%04x)\n", device->product_type, device->hardware_model, device->board_id, device->chip_id);
+
+    const struct irecv_device_info *info = irecv_get_device_info(client);
+    if (!info)
+    {
+        ERROR("Could not get iDevice info!\n");
+        return -1;
+    }
+    DEBUG("ECID: %"PRId64" (decimal), %"PRIX64" (hexadecimal)\n", info->ecid, info->ecid);
+    DEBUG("IMEI: %s\n", info->imei);
+
+    INFO("Successfully connected to device!\n");
     return 0;
 }
